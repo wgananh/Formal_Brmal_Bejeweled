b419aac6d9ea3880c11d149f22dcee4c9b78f9eb
supertailcat <hcyupy@outlook.com>
2020-05-21 14:36:37 +0800

    王汉功：加了rank.txt

--- crankdlg.cpp
+++ crankdlg.cpp
@@ -7,28 +7,90 @@ CRankDlg::CRankDlg(QWidget *parent) :
 {
     cranklogic = new CRankLogic();
     cranklogic->updateRank();
-//    for(int i = 0; i < 10; i++)
-//        for(int j = 0; j < 3; j++)
-//            ui->tableWidget->setItem(i, j, new QTableWidgetItem(""));
-    qDebug() << ui->
     ui->setupUi(this);
 
 }
 
 void CRankDlg::showRank()
 {
-//    cranklogic->updateRank();
-    for(int i = 0; i < cranklogic->crankdao->totalNumber; i++)
+    QString name;
+    QString time;
+    QString grade;
+    switch (cranklogic->crankdao->totalNumber)
     {
-        const QString name = QString::fromLatin1(cranklogic->crankdao->ranks[i]->strName);
-//        qDebug() << this->ui->tableWidget->item(1, 1)->text();
-        this->ui->tableWidget->item(i, 0)->setText(name);
-        const QString time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[i]->nTime));
-        this->ui->tableWidget->item(i, 1)->setText(time);
-        const QString grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[i]->nGrade));
-        this->ui->tableWidget->item(i, 2)->setText(grade);
+    case 10:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[9]->strName);
+        ui->label_28->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[9]->nTime));
+        ui->label_29->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[9]->nGrade));
+        ui->label_30->setText(grade);
+    case 9:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[8]->strName);
+        ui->label_25->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[8]->nTime));
+        ui->label_26->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[8]->nGrade));
+        ui->label_27->setText(grade);
+    case 8:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[7]->strName);
+        ui->label_22->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[7]->nTime));
+        ui->label_23->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[7]->nGrade));
+        ui->label_24->setText(grade);
+    case 7:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[6]->strName);
+        ui->label_19->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[6]->nTime));
+        ui->label_20->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[6]->nGrade));
+        ui->label_21->setText(grade);
+    case 6:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[5]->strName);
+        ui->label_16->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[5]->nTime));
+        ui->label_17->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[5]->nGrade));
+        ui->label_18->setText(grade);
+    case 5:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[4]->strName);
+        ui->label_13->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[4]->nTime));
+        ui->label_14->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[4]->nGrade));
+        ui->label_15->setText(grade);
+    case 4:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[3]->strName);
+        ui->label_10->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[3]->nTime));
+        ui->label_11->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[3]->nGrade));
+        ui->label_12->setText(grade);
+    case 3:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[2]->strName);
+        ui->label_7->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[2]->nTime));
+        ui->label_8->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[2]->nGrade));
+        ui->label_9->setText(grade);
+    case 2:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[1]->strName);
+        ui->label_4->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[1]->nTime));
+        ui->label_5->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[1]->nGrade));
+        ui->label_6->setText(grade);
+    case 1:
+        name = QString::fromLatin1(cranklogic->crankdao->ranks[0]->strName);
+        ui->label_1->setText(name);
+        time = QString::fromStdString(to_string(cranklogic->crankdao->ranks[0]->nTime));
+        ui->label_2->setText(time);
+        grade = QString::fromStdString(to_string(cranklogic->crankdao->ranks[0]->nGrade));
+        ui->label_3->setText(grade);
+    case 0:
+        this->show();
     }
-    this->show();
 }
 
 CRankDlg::~CRankDlg()
