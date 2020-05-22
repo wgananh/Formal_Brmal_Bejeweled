31cbd59f4bef113acc18196a137d03946230c6b6
supertailcat <hcyupy@outlook.com>
2020-05-22 14:28:33 +0800

    于沛尧：增加了gamelogic的函数定义。

--- cgamelogic.h
+++ cgamelogic.h
@@ -9,6 +9,10 @@ public:
 private:
     int species;//宝石种类数
     int m_aMap[8][8];//0代表空，1~n代表第n种宝石
+    int hint1x;
+    int hint1y;
+    int hint2x;
+    int hint2y;
 
     void BuildMap();//生成不三连的图形
     bool swap(int a, int b, int m, int n);//交换（不相邻或无法消子，返回false）需要调用eliminate；也可以不这样写
