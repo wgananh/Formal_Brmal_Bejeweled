3d845436b0eccb5552d105351487d2788815a8a4
supertailcat <hcyupy@outlook.com>
2020-05-26 18:14:51 +0800

    于沛尧：加了一下关于图片鼠标相应的东西，全局变量也修改了

---
+++ myqpixmap.cpp
@@ -0,0 +1,6 @@
+#include "myqpixmap.h"
+
+MyQPixmap::MyQPixmap()
+{
+
+}
---
+++ myqpixmap.h
@@ -0,0 +1,13 @@
+#ifndef MYQPIXMAP_H
+#define MYQPIXMAP_H
+
+#include <QObject>
+
+class MyQPixmap : public QPicmap
+{
+    Q_OBJECT
+public:
+    MyQPixmap();
+};
+
+#endif // MYQPIXMAP_H
