2f5b61a4b660d140d7e20e13be6a1d33c9802b57
wgananh <17531865118@163.com>
2020-05-26 09:10:37 +0800

    王汉功：重新确定了species的值，重新开始游戏的按钮。生成不三连的地图、计时、暂停、继续游戏等功能均已实现

--- cgamelogic.h
+++ cgamelogic.h
@@ -15,23 +15,25 @@ private:
     //*******请在写 “交换函数（swap）” 时，务必判断该布尔变量game_running是否为true，只有为true是才能继续操作*********
     bool game_running; //游戏运行状态，该布尔变量默认为true,当在cgamelog界面中点击“暂停游戏”按钮时该值为false。true时表示游戏处于运行状态
 
-    int species;//宝石种类数
-    int m_aMap[8][8];//0代表空，1~n代表第n种宝石
-    int mapsize=64,maprownum=8,mapcolnum=8;
+    int species=8;//一共有8中宝石种类数
+    int m_aMap[8][8];//0代表空，1~n代表第n种宝石（宝石号码取值为1-8）
+    int mapsize=64,maprownum=8,mapcolnum=8; //定义地图尺寸，行数、列数
     int hint1x;
     int hint1y;
     int hint2x;
     int hint2y;
 
 public:
-    void BuildMap();//生成不三连的图形
+    void BuildMap();//生成不三连的图形，用于camglog中调用来初始化地图，以下消子、交换、下移、产生新子等函数需要地图时只需调用 "m_aMap[8][8]"
     bool checkmap(); //只是为了再次判断地图中是否有相邻的三个宝石相同，只用于BuildMap()中引用,写其他代码时可忽略此函数。
+
     bool swap(int a, int b, int m, int n);//交换（不相邻或无法消子，返回false）需要调用eliminate；也可以不这样写
     bool eliminate();//消子
     void down();//下移
     void produce();//产生新子
 
-    void setgame_running(bool game_running); //cgamedlg中用于设置游戏是否暂停
+    void setgame_running(bool game_running); //cgamedlg中用于设置游戏是否暂停，写其他代码时可忽略本函数。
+                                             //写消子、交换、下移、产生新子等函数时只用判断布尔变量 "game_running"是否为真
 
     void newGrade();//不太清楚怎么做
     bool isWin();//胜负判断？或者升级？
